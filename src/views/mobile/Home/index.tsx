138ca23eef749595afdc4a0bb29e6e7eff1d0ff4
root <root@qq.com>
2022-10-19 15:23:41 +0800

    add _input reset

--- src/views/mobile/Home/index.tsx
+++ src/views/mobile/Home/index.tsx
@@ -1,14 +1,26 @@
-import {FC, memo, ReactElement} from 'react'
+import React, {FC, memo, ReactElement, useCallback, useState} from 'react'
 import Baseweb from '@/components/baseContainer/webwrap'
 import {useTranslation} from 'react-i18next'
+import Icon from "@/components/Icon";
+import icon_search from "@/assets/images/mp/icon_search.png";
+import './index.scss'
 
 const Home: FC = (): ReactElement => {
     const {t} = useTranslation()
+    const [inputVal, setInputVal] = useState<string>('')
+    const inputChange = useCallback(
+        (e) => {
+            const value = e?.target?.value ?? ''
+            setInputVal(value)
+        },
+        []
+    )
     return (
-        <div>
-            <h1>
-                {t('common.header.nav.home')}
-            </h1>
+        <div className='home-container'>
+            <div className='input-external flex-row-start-center'>
+                <Icon src={icon_search} height="0.4rem"></Icon>
+                <input value={inputVal} onChange={inputChange} placeholder="搜索账号/交易哈希/区块编号"/>
+            </div>
 
         </div>
     )
