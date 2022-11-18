b557e9b8c3b17463f0d7839d1cd072c7c10d583f
Waymilk <745309089@qq.com>
2022-11-18 17:40:41 +0800

    修改block跳转

--- src/components/BlockList/index.tsx
+++ src/components/BlockList/index.tsx
@@ -18,10 +18,12 @@ const BlockList: React.FunctionComponent<ListProps> = ({showPage = false,data}) 
          return(
           <tr className="latest-block-list-item text-align-between" key={item.id}>
             <td className="item-title main-color"> <span className="pointer" onClick={()=>{
-              history.push({ pathname: `/block-detail/${item?.block_num }` });
+              history.push(`/block-detail/${item?.block_num }`);
             }}>{`#${item?.block_num}`}</span></td>
             <td className="item.time">{handleTime(item?.timestamp)}</td>
-            <td><span className="item-type pointer main-color">{item?.producer}</span></td>
+            <td><span className="item-type pointer main-color" onClick={()=>{
+              history.push(`/producer-detail/${item?.producer }`);
+            }}>{item?.producer}</span></td>
             <td className="item-transaction">交易<span className="text-red">{item?.transactions.length}</span>笔</td>
           </tr>
           )
