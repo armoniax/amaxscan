5c5ef10bd95934a4113b6247ce5de0af5195a8b7
745309089@qq.com <745309089@qq.com>
2022-12-01 17:01:49 +0800

    页面冲突修改

--- src/components/BlockList/index.tsx
+++ src/components/BlockList/index.tsx
@@ -19,13 +19,7 @@ const BlockList: React.FunctionComponent<ListProps> = ({showPage = false,data}) 
           <tr className="latest-block-list-item text-align-between" key={item.id}>
             <td className="item-title main-color"> <span className="pointer" onClick={()=>{history.push(`/block-detail/${item?.block_num }`);}}>{`#${item?.block_num}`}</span></td>
             <td className="item.time">{handleTime(item?.timestamp)}</td>
-<<<<<<< HEAD
             <td><span className="item-type pointer main-color" onClick={()=>{history.push(`/producer-detail/${item?.producer}`)}}>{item?.producer}</span></td>
-=======
-            <td><span className="item-type pointer main-color" onClick={()=>{
-              history.push(`/producer-detail/${item?.producer }`);
-            }}>{item?.producer}</span></td>
->>>>>>> b557e9b8c3b17463f0d7839d1cd072c7c10d583f
             <td className="item-transaction">交易<span className="text-red">{item?.transactions.length}</span>笔</td>
           </tr>
           )
