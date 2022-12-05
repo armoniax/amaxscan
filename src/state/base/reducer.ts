4be1a627a94c5865b0094d0cc06293a93d5f8c9f
Waymilk <745309089@qq.com>
2022-12-05 17:45:10 +0800

    扫码登陆

--- src/state/base/reducer.ts
+++ src/state/base/reducer.ts
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { AppThunk } from '@/state';
 import { setUserInfo, AccountStatus } from './actions';
 import {network, getScatter, getClient, initLink} from '@/utils/client'
-import {IdentityProof} from "@amax/anchor-link";
+import {IdentityProof,ChainId,APIError} from "@amax/anchor-link";
 
 const initialState = {
     account: {
@@ -13,6 +13,20 @@ const initialState = {
     }
 };
 
+export const blockchains = [
+  {
+    chainId: network.chainId,
+    name: network.blockchain,
+    rpcEndpoints: [
+      {
+        protocol: network.protocol,
+        host: network.host,
+        port: 0,
+      },
+    ],
+  },
+];
+
 const BaseInfoSlice = createSlice({
     name: 'base',
     initialState,
@@ -22,6 +36,57 @@ const BaseInfoSlice = createSlice({
         }
     },
 });
+
+
+export async function verifyProof(link, identity) {
+  // Generate an array of valid chain IDs from the demo configuration
+  const chains = blockchains.map(chain => chain.chainId);
+
+  // Create a proof helper based on the identity results from anchor-link
+  const proof = IdentityProof.from(identity.proof);
+
+  // Check to see if the chainId from the proof is valid for this demo
+  const chain = chains.find(id => ChainId.from(id).equals(proof.chainId));
+  if (!chain) {
+    throw new Error('Unsupported chain supplied in identity proof');
+  }
+
+  // Load the account data from a blockchain API
+  // let account: API.v1.AccountObject;
+  let account = null;
+  try {
+    account = await link.client.v1.chain.get_account(proof.signer.actor);
+  } catch (error) {
+    if (error instanceof APIError && error.code === 0) {
+      throw new Error('No such account');
+    } else {
+      throw error;
+    }
+  }
+
+  // Retrieve the auth from the permission specified in the proof
+  const auth = account.getPermission(proof.signer.permission).required_auth;
+
+  // Determine if the auth is valid with the given proof
+  const valid = proof.verify(auth, account.head_block_time);
+
+  // If not valid, throw error
+  if (!valid) {
+    throw new Error('Proof invalid or expired');
+  }
+
+  // Recover the key from this proof
+  const proofKey = proof.recover();
+
+  // Return the values expected by this demo application
+  return {
+    account,
+    proof,
+    proofKey,
+    proofValid: valid,
+  };
+}
+
 export const getUserInfo =
     (connectorType:any): AppThunk =>
     async dispatch => {
@@ -49,18 +114,24 @@ export const getUserInfo =
                 }));
             }else{
                 const link = initLink();
+                console.log(link,'linklink');
+
                 const identity = await link.login("anchor-link-demo");
-                console.log(identity)
-                const proof = IdentityProof.from(identity.proof);
-                const account = await link.client.v1.chain.get_account(proof.signer.actor);
-                console.log(identity.session,account)
-                dispatch(setUserInfo({
-                    kyc: false,
-                    name: account.account_name.toString(),
-                    publicKey: '',
-                    status: 0
-                }));
-                console.log('ssssssssssss',account.account_name.toString())
+                const { account, proof, proofKey, proofValid } = await verifyProof(
+                  link,
+                  identity,
+                );
+                console.log(account, proof, proofKey, proofValid,'ssss');
+                // return
+                // const account = await link.client.v1.chain.get_account(proof.signer.actor);
+                // console.log(identity.session,account)
+                // dispatch(setUserInfo({
+                //     kyc: false,
+                //     name: account.account_name.toString(),
+                //     publicKey: '',
+                //     status: 0
+                // }));
+                // console.log('ssssssssssss',account.account_name.toString())
             }
 
         }catch(e){
